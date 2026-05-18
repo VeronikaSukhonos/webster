04fc5de8ab79cb351f940a32c6c5adfa5905cb63
VeronikaSukhonos <veronika.sukhonos@gmail.com>
2026-05-18 21:48:10 +0300

    fix for deployment

--- frontend/src/components/editor/EditorMenu.tsx
+++ frontend/src/components/editor/EditorMenu.tsx
@@ -1,9 +1,9 @@
-import type { Project, Template } from '@store/editorSlice';
+import type { ProjectResponse, TemplateResponse } from '@mytypes/responseTypes';
 
-export const ProjectMenu = ({ project }: { project: Project }) => {
+export const ProjectMenu = ({ project }: { project: ProjectResponse }) => {
   return <div>Menu TODO {JSON.stringify(project)}</div>;
 };
 
-export const TemplateMenu = ({ template }: { template: Template }) => {
+export const TemplateMenu = ({ template }: { template: TemplateResponse }) => {
   return <div>Menu TODO {JSON.stringify(template)}</div>;
 };
